d8201c9b3ef6ecb7880faace15879b0528c97f45
redoy1102 <hridoyvlog9@gmail.com>
2024-02-28 12:50:16 +0600

    L - The Brothers done

---
+++ Sheet 1/L/l.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    char a[200], b[200], c[200], d[200];
+
+    cin >> a >> b;
+    cin >> d >> d;
+    
+    if(strcmp(b, d) == 0) 
+    {
+        cout << "ARE Brothers" << endl;
+    }
+    else 
+    {
+        cout << "NOT" << endl;
+    }
+
+    return 0;
+}
